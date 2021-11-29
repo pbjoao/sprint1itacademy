dc58682c7b63d470d55744981a0af03c91a9e1f8
pedrojoao <pbjoao7@gmail.com>
2021-11-29 10:14:00 +0100

    actu29/11/21

--- Entrega 1.1/Sprint 1 Entregas 1.1.js
+++ Entrega 1.1/Sprint 1 Entregas 1.1.js
@@ -9,11 +9,11 @@ function saludar(nombre){
     console.log("El nombre del usuario es: " + nombre);
 }
 
-console.log(saludar("Manuel"));
+saludar("Manuel");
 
 
 // N2 ex01
-let nombre = "Jose";
+nombre = "Jose";
 let apellido = "Ramirez";
 
 console.log(`Hola ${nombre} ${apellido}`);
@@ -28,86 +28,36 @@ function valor(numero){
     console.log(`El valor es ${numero}`);
 }
 
-console.log(valor(3));
+valor(3);
 
 
 // N3 ex01
-function loop1(){
-    for(i=0; i<=10; i++){
-        console.log(i);       
-    }
+
+var arrayLoops =[];
+
+function loop(){
+
+    for(i=0; i<=9; i++){
+
+        console.log(i);
+    }   
     console.log("final loop")
  }
 
- function loop2(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
+ while(arrayLoops.length<10){
 
- function loop3(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
+    arrayLoops.push(loop());
  }
- function loop4(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
- function loop5(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
+ 
+arrayLoops[i];
 
- function loop6(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
 
- function loop7(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
 
- function loop8(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
-
- function loop9(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
-
- function loop10(){
-    for(i=0; i<=10; i++){
-        console.log(i);
-    }
-    console.log("final loop")
- }
-
-loops = [loop1(), loop2(), loop3(), loop4(), loop5(), loop6(), loop7(), loop8(), loop9(), loop10()];
-
-for(let x in loops){
-    console.log(x);
-}
+// for(let x in loops){
+//     console.log(x);
+// }
 
 
 // N3 ex02
-(function(nombre){console.log(nombre)})("jose");
+var exN3 = (function(nombre){console.log(nombre)})("jose");
 
-//jjjj
